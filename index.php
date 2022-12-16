8ee7a4ebbabc90f41f6348c2013395ede1e4a76f
gagankrmahto <60207668+gagankrmahto@users.noreply.github.com>
2022-12-16 14:05:18 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 print("<br>");
 print `ip addr (ip a)`;
-print("<br><H3>Hello Interns</H3> This is my IP : ")
+print("<br><H3>Hello Interns</H3> This is my IP : ");
 print `hostname -I | awk '{print $1}'`;
 ?>
