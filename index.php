db60afb7729b056751c74ee940fa9975252d3e99
gagankrmahto <60207668+gagankrmahto@users.noreply.github.com>
2022-12-16 14:03:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
-Hello world
 <?php
-print `ip addrress`;
-print `ifconfig -a`;
 print("<br>");
 print `ip addr (ip a)`;
+print("<br><H3>Hello Interns</H3> This is my IP : ")
 print `hostname -I | awk '{print $1}'`;
 ?>
